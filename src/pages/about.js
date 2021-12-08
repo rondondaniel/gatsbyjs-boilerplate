0a1770863dcefa83fe763ae1b9c82079bc31bd58
Daniel Rondon <rondondaniel@gmail.com>
2021-12-08 23:33:31 +0100

    Doing Tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <Link to="/">Back to home page</Link>
+    </main>
+  )
+}
+
+export default AboutPage

3e4fe17a8e2b5d59a6d0cd02e547e8b41a1b05ec
Daniel Rondon <rondondaniel@gmail.com>
2021-12-09 14:10:30 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "sandbox-project",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };

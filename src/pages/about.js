3434ad23c9e6e07b482cc1f934525abee8e4b654
Daniel Rondon <rondondaniel@gmail.com>
2021-12-09 13:48:10 +0100

    Finished Gatsby Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,11 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
+    <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      <Link to="/">Back to home page</Link>
-    </main>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcom to my site</h1>
-      <p>I'm making this following gatsby tuto</p>
-      <Link to="/about">About</Link>
-    </main>
+     <Layout pageTitle="Home Page">
+       <p>I'm making this by following the Gatsby Tutorial.</p>
+     </Layout>
   )
 }
 

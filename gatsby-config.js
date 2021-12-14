76c8fa9dee0d5a9a54610101b55979fc2cdad002
Daniel Rondon <rondondaniel@gmail.com>
2021-12-14 19:07:33 +0100

    Working with GraphQL Queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
     title: "sandbox-project",
+    siteUrl: "https://www.yourdomain.tld",
   },
   plugins: [
     "gatsby-plugin-image",

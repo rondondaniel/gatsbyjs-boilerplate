6851f7dfd4c25c6d576908a6155ecf1275ebcba1
Daniel Rondon <rondondaniel@gmail.com>
2021-12-24 18:00:12 +0100

    Finished Gatsby Tutorial Part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "sandbox-project",
+    title: "My sandbox Project",
     siteUrl: "https://www.yourdomain.tld",
   },
   plugins: [
@@ -14,5 +14,6 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
